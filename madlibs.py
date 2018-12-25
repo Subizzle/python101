4702cce02e46ef3572a40f8c9e9a128ab33d832a
Subizzle <46074929+Subizzle@users.noreply.github.com>
2018-12-25 15:07:56 -0500

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -5,13 +5,11 @@
 #    Without practice, my __(noun)__ would probably not even work.
 #    My code is getting more __(adjective)__ every single day!
 
-
 # TODO: Prompt the user for parts of speech and store them in variables
 
 verb = input('Please enter a verb: ')
 noun = input('Please enter a noun: ')
 adjective = input('Please enter a adjective: ')
-
 
 # TODO: Output the template to the screen with the blanks filled out with what the user stated
 print('I enjoy practice! I find it helps me to', verb, 'better.')
