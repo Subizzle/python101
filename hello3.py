eb3cfb73e12137a53f301dd2523fb06b5d939fb8
Subizzle <46074929+Subizzle@users.noreply.github.com>
2018-12-26 16:57:50 -0500

    Create hello3.py

---
+++ hello3.py
@@ -0,0 +1,5 @@
+first_name = input('What is your first name? ')
+print('Hello,' , first_name)
+if first_name == 'Chris':
+    print(first_name, 'is learning Python.')
+print('Have a great day {}!'.format(first_name))
