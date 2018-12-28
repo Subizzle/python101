56cf300ee0adf9a0dbe445bc69b381a7e137df27
Subizzle <46074929+Subizzle@users.noreply.github.com>
2018-12-28 13:39:17 -0500

    Update masterticket.py

--- masterticket.py
+++ masterticket.py
@@ -1,62 +1,27 @@
-import math
-     
 TICKET_PRICE = 10
-
+SERVICE_CHARGE = 2
 tickets_remaining = 100  
+def calculate_price(TICKET_PRICE, number_of_tickets):
+    return (TICKET_PRICE * number_of_tickets) + SERVICE_CHARGE
 
-# Run this code continually until we run out of tickets. 
 
 while tickets_remaining >= 1:
-
-    # Output how many tickets are remaining using the tickets_remaining variable. 
-    
     print("Hello Python fans! Welcome to the secret ticket site!\nThere are currently {} tickets remaining for purchase!".format(tickets_remaining))
-    
-    # Gather the users name and assign it to a new variable.
-    
-    user_name = input("With whom am I typing with today? ")
-    
-    # Prompt the user by name and ask them how many tickets they would like.
-    
-    tickets_requested = int(input("{} how many tickets would you like to purchase? ".format(user_name)))
-    
-    # Calculate the price (number of tickets multiplied by the price) and assign it to a variable.
-    
-    def amount_due(TICKET_PRICE, tickets_requested):
-        if tickets_requested < 1:
-            raise ValueError("At least 1 should be entered")
-        return math.ceil(TICKET_PRICE * tickets_requested)  
-    
-    # Output the price to the screen.
-    
-    total_cost = amount_due(TICKET_PRICE, tickets_requested)
-    
-    print("{} the total price would come to ${}.".format(user_name, total_cost))
-    
-    # Prompt the user if they want to proceed Y/N
-    
-    confirm = input("At this time {} can I confirm you want to purchase {} tickets for ${}?\nPlease respond yes or no. ".format(user_name, tickets_requested, total_cost))
-    
-    # If they want to proceed...
-    
-    if  confirm.lower() == "y":
-    
-        # Print out to screen "SOLD!" to confirm purchase
-        
-        print("{} tickets for ${} SOLD to {}!".format(tickets_requested, total_cost, user_name))
-        
-        # And then dedeuct from the total ticket amount
-        
-        tickets_remaining -= tickets_requested
-    
-    # Otherwise ...
-    
+    name = input("With whom am I typing with today? ")
+    num_tickets = input("{} how many tickets would you like to purchase? ".format(name))
+    try:
+        num_tickets = int(num_tickets)
+        if num_tickets > tickets_remaining:
+            raise ValueError("There are only {} tickets remaining for purchase.".format(tickets_remaining))
+    except ValueError as err:
+        print("{} we have an issue! {}".format(name, err))
     else:
-        
-        print("Thank you for your interest {}!".format(user_name))
-    
-        # Thank them by name.
-        
-# Notify user that the tickets are currently sold out.
-
-print(" We're sorry, all tickets for this event have been SOLD OUT!")
+        amount_due = calculate_price(TICKET_PRICE,num_tickets)
+        print("{} the total price would come to ${}.".format(name, amount_due))
+        confirm = input("At this time {} can I confirm you want to purchase {} tickets for ${}?\nPlease respond yes or no. ".format(name, num_tickets, amount_due))
+        if  confirm.lower() == "y":
+            print("{} tickets for ${} SOLD to {}!".format(num_tickets, amount_due, name))
+            tickets_remaining -= num_tickets
+        else:
+            print("Thank you for your interest {}!".format(name))
+print("We're sorry, all tickets for this event have been SOLD OUT!")
